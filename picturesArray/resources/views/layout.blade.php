fd78fa380d636a0e5690224fe0a55c987842faff
unknown <lisaveta256@gmail.com>
2019-09-30 14:16:38 +0300

    a

---
+++ picturesArray/resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>pictures</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/style.css">
+   
+
+  
+    </head>
+    <body>
+@include('parts.header')
+
+ @yield('content')
+ 
+@include('parts.footer')
+    
+    </body>
+</html>
---
+++ picturesArray/resources/views/parts/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+  <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" href="/">Home</a>
+  </li>
+  <li class="nav-item">
+    <a id='recycle' class="nav-link" href="/bank" onclick="">Recycle</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" >Salles</a>
+  </li>
+
+</ul>
+  </header>
